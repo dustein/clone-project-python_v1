c957ea19a26c763cfb0b94999b80f5bcfc041d1f
dustein <steindu@gmail.com>
2024-02-20 20:18:10 -0300

    nao foi

--- functions_to_think.py
+++ functions_to_think.py
@@ -1,6 +1,23 @@
+create_job (userOwnerId: string, jobDescription: object) {
+  const jobId = JOB#${new UILD()};
 
+  await dynamodb.put({
+    Entity: "Job", // PK do GSI_1
+    id: jobId, // PK principal da tabela
+    userOwnerId,
+    // GSI_1_SK: undefined // não precisa adicionar se vazio
+    ...jobDescription
+  })
+}
 
-def CreateUser(data_user):
-  table.put_item(
-    
-  )
+create_user (accountId: string, userDescription: object) {
+  const userId = USER#${new UILD()};
+  
+  await dynamodb.put({
+    Entity: "User", // PK do GSI_1
+    id: userId, // PK principal da tabela
+    accountId,
+    GSI_1_SK: accountId,
+    ...userDescription
+  })
+}
