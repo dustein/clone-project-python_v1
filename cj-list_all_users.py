4d3227e47a9a58f8e8e030cc437f0fc3fc217739
dustein <steindu@gmail.com>
2024-02-26 10:52:18 -0300

    list_all_users working good

--- cj-list_all_users.py
+++ cj-list_all_users.py
@@ -1,14 +1,16 @@
 import json
 import boto3
+from boto3.dynamodb.conditions import Attr
 from custom_encoder import CustomEncoder
-# client = boto3.client('dynamodb')
-dynamodbTableName = 'clone-jobs1v'
+
+dynamodbTableName = 'clone-jobs'
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(dynamodbTableName)
 
 def lambda_handler(event, context):
 
-  data = table.scan()
+  # data = table.scan()
+  data = table.scan(FilterExpression=Attr('PK').begins_with('USER#'))
   res_data = data['Items']
 
   while 'LastEvaluatedKey' in data:
